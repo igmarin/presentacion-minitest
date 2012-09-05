1cc896017165e0e34c30665389ecf5bc0b6f5c2d
Ismael G Marin C <ismael.marin@gmail.com>
2012-09-04 19:13:24 -0500

    More methods

--- Ejemplo1/ejemplo1_minitestpec.rb
+++ Ejemplo1/ejemplo1_minitestpec.rb
@@ -1,9 +1,31 @@
 require 'minitest/spec'
 require 'minitest/autorun'
+require 'minitest/pride'
 
 
-describe Integer do
-  it "shoul be equal" do
+describe Fixnum do
+  it "is class Fixnum" do
+    1.must_be_instance_of Fixnum
+  end
+
+  it "is not a String" do
+    1.wont_be_kind_of String
+  end
+
+  it "is of the class or superclass" do
+    1.must_be_kind_of Integer
+  end
+  it "is equal" do
     1.must_equal 1
+  end
+
+  it "is bigger than" do
+    12.must_be :>, 10
+  end
+  it "Is not equal" do
+    1.wont_equal 2
+  end
+  it "is nil" do
+    nil.must_be_nil nil
   end
 end
--- Ejemplo1/ejemplo1_minitestunit.rb
+++ Ejemplo1/ejemplo1_minitestunit.rb
@@ -1,8 +1,12 @@
 require 'minitest/unit'
 require 'minitest/autorun'
+require 'minitest/pride'
 
-class TestInteger < MiniTest::Unit::TestCase
-  def test_number_is_equal
+class TestFixnum < MiniTest::Unit::TestCase
+  def test_number_is_not_equal
     refute_equal 2, 1
+  end
+  def test_is_class_fixnum
+    assert_instance_of Fixnum, 1
   end
 end
